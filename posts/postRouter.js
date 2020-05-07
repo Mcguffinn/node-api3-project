f22108a36b6dee8ca8b6ccdaefd27a3f66ad7ab8
Mcguffinn <deronviledwin@gmail.com>
2020-05-07 12:44:50 -0400

    added postsRouter for stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -11,22 +11,47 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, async(req, res) => {
+  try{
+    const proc = await Posts.getById(req.params.id);
+    res.status(200).json({proc})
+  }catch(err){
+    res.status(200).json({message: "No id was found...", err})
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, async(req, res) => {
+  try{
+    const proc = await Posts.remove(req.params.id);
+    res.status(200).json({proc})
+  }catch{
+    res.status(200).json({message: "No id was found...", err})
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, async(req, res) => { 
+  try{
+    await Posts.update(req.params.id, req.body.text)
+  }catch{
+    res.status(200).json({message: `This post ${req.params.id} was not updated...`, err})
+  }
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
   // do your magic!
+  Posts.getById(req.params.id)
+  .then( x =>{
+    if (x) {
+      next()
+    } else {
+      res.status(404).json({message: 'invalid user id'})
+    }
+  })
+  .catch(err =>{
+    res.status(500).json({message: "Server error", error})
+  })
 }
 
 module.exports = router;
